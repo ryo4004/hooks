864a85190f2369dbee668566063dac8a340544d1
ryo4004 <kato@ryo4004.net>
2020-05-28 00:22:45 +0900

    Update practice

--- src/practice/useCallbackEvent.js
+++ src/practice/useCallbackEvent.js
@@ -4,11 +4,8 @@ const Callback = () => {
   const [count, setCount] = useState(0);
 
   const increment = useCallback(() => {
-    console.log('[useCallbackEvent] increment');
     setCount(prev => prev + 1);
   }, []);
-
-  console.log('[useCallbackEvent] render');
 
   return (
     <div className='counter'>
